1404d348c2d46d81f77f338e1a7bfcb88f609b27
DEVILXID <rishikeshanand51@gmail.com>
2026-03-24 00:12:11 +0530

    Update 00gm.js

--- Priyansh/commands/00gm.js
+++ Priyansh/commands/00gm.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
      react.includes("Morning") ||
 react.includes("morning")) {
     var msg = {
-        body: `★━━━━━━━━━━━━━★😍𝐔𝐭𝐡𝐣𝐚𝐨 𝐊𝐮𝐦𝐛𝐡𝐤𝐚𝐫𝐚𝐧😒😍★━━━━━━━━━━━━━★`,attachment: fs.createReadStream(__dirname + `/noprefix/gmm.jpg`)
+        body: `★━━━━━━━━━━━━━★😍 𝐔𝐓𝐇 𝐉𝐀𝐎𝐎 𝐊𝐔𝐌𝐁𝐊𝐀𝐑𝐀𝐍𝐎 𝐊𝐈 𝐀𝐔𝐋𝐀𝐃𝐎😒😍★━━━━━━━━━━━━━★`,attachment: fs.createReadStream(__dirname + `/noprefix/gmm.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😈", event.messageID, (err) => {}, true)
